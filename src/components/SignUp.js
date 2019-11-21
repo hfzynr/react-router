cb2db84c810504e309e7a446ccc27f7b12c7f8a7
Hafiz Yanuar Ibrahim <hafizyanuar95@gmail.com>
2019-11-21 10:39:16 +0700

    add tab user

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -163,11 +163,6 @@ function SignUp(props) {
             </Button>
 
             <Grid container justify="flex-end">
-            <Grid item>
-                <Link href="#" variant="body2">
-                Already have an account? Sign in
-                </Link>
-            </Grid>
             </Grid>
         </form>
         </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Axios from 'axios'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
 
-export default function User() {
-    return (
-        <div>
-            <p>
-                Ini User
-            </p>
-        </div>
-    )
+export default class User extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            data:[]
+        }
+    }
+    
+    componentDidMount = () => {
+        Axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            this.setState({data: response.data })
+        })
+        
+    } 
+
+    render() {
+        return (
+            <div
+            style={{ display: 'flex', flexWrap:'wrap'}}>
+                {this.state.data.map((item, key) => {
+                    return (
+                        <div>
+                            <Card 
+                            style={{ padding:'10px', margin: "10px"}}
+                            key={key}>
+                                <CardContent>
+                                    <Typography variant="h5" component="h2">
+                                        {item.name}
+                                    </Typography>
+                                    <Typography variant="body2" component="p">
+                                        {item.username}
+                                    </Typography>
+                                    <Typography variant="body2" component="p" color="textSecondary">
+                                        Email : {item.email}
+                                    </Typography>
+                                    <Typography variant="body2" component="p" color="textSecondary">
+                                        Phone : {item.phone}
+                                    </Typography>
+                                </CardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary">Write Something...</Button>
+                                    </CardActions>
+                            </Card>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
 }
+
+
+////////////////////////////////////////
+    // render() {
+    //     return (
+    //         <div>
+    //     {this.state.data.map((item, index) => {
+    //         return (
+    //             <div>
+    //                 <Card>
+    //                     <CardContent>
+    //                         <Typography>
+    //                             Kambing
+    //                         </Typography>
+    //                     </CardContent>
+    //                 </Card>
+    //             </div>
+    //         )
+    //     )}
+    // }
+    // </div>
+    // )
+    // }
